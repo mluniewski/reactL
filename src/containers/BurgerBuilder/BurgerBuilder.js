71a7d287b7704157ef41224d708589746a700724
Luniewski <mateusz.luniewski@accenture.com>
2020-03-13 12:42:05 +0100

    version3

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as burgerBuilderActions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 
 
@@ -48,6 +48,7 @@ import * as burgerBuilderActions from '../../store/actions/index';
     }
 
     purchaseContinueHandler = () => {
+        this.props.onInitPurchase();
         this.props.history.push('/checkout')           
     }
 
@@ -95,17 +96,18 @@ import * as burgerBuilderActions from '../../store/actions/index';
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        error: state.error
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        error: state.burgerBuilder.error
     }
 }
 
 const mapDispatchToProps= dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
-        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit())
     }
 }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -87,13 +87,12 @@ class ContactData extends Component {
                                 {value: 'cheapest', displayValue: 'Cheapest'}
                             ]
                         },
-                        value: '',
+                        value: 'fastest',
                         validation: {},                       
                         valid: true
                     }
         },
         formIsValid: false,
-        loading: false
     }
 
     orderHandler = (event) => {
@@ -173,7 +172,7 @@ class ContactData extends Component {
                     <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
              </form>
         );
-        if(this.state.loading) {
+        if(this.props.loading) {
             form = <Spinner/>
         }
         return (
@@ -188,13 +187,17 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 };
 
 const mapDispatchToProps = dispatch => {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+
+    }
 }
 
-export default connect(mapStateToProps) (withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps) (withErrorHandler(ContactData, axios));
